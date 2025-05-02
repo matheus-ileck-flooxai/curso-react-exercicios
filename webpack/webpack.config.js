c3ee09a05b497ceb646024afd119be3e41b5ec1e
Matheus-Ileck-flooxai <matheus.ileck.flooxai@gmail.com>
2025-05-02 11:57:12 -0300

    feat: configure output folder

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -3,7 +3,11 @@ const webpack = require('webpack')
 module.exports = {
 
     mode: 'development',
-    entry: './src/principal.js'
+    entry: './src/principal.js',
+    output: {
+        filename: 'principal.js',
+        path: __dirname + '/public'
+    }
     
 
 }
