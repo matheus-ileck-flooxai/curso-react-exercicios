53a3066ddf5eaf0261698d0b7ebb21c4a172a849
Matheus-Ileck-flooxai <matheus.ileck.flooxai@gmail.com>
2025-05-05 09:14:04 -0300

    feat: configure development and production build

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,13 +1,25 @@
+const modoDev = process.env.NODE_ENV !== 'production'
 const webpack = require('webpack')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
 
 module.exports = {
 
-    mode: 'development',
+    mode: modoDev ? 'development' : 'production',
     entry: './src/principal.js',
     output: {
         filename: 'principal.js',
         path: __dirname + '/public'
+    },
+    optimization: {
+        minimizer: [
+            new UglifyJsPlugin({
+                cache: true,
+                parallel: true
+            }),
+            new OptimizeCSSAssetsPlugin({})
+        ]
     },
     plugins: [
         new MiniCssExtractPlugin({
@@ -19,7 +31,7 @@ module.exports = {
             test: /\.s?[ac]ss$/,
             use: [
                 MiniCssExtractPlugin.loader,
-              //'style-loader', //Adiciona CSS a DOM ijetando a tag <style>
+                //'style-loader', //Adiciona CSS a DOM ijetando a tag <style>
                 'css-loader', //Interpreta @import, url()...
                 'sass-loader'
             ]
