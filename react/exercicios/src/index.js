a3791c65d2a413e1e936b2da56e6c3a4704fecf3
Matheus-Ileck-flooxai <matheus.ileck.flooxai@gmail.com>
2025-05-05 10:22:37 -0300

    feat: add Primeiro component

--- react/exercicios/src/index.js
+++ react/exercicios/src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDom from 'react-dom'
+import Primeiro from "./componentes/Primeiro";
 
-ReactDom.render(<h1>React</h1>, document.getElementById('root'))
+ReactDom.render(<Primeiro />, document.getElementById('root'))
 
